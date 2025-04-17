68ef95cb20c09507e8796a945c065c3041c0465c
Marc Peters <mr.marc.peters@gmail.com>
2025-04-17 16:48:35 -0700

    [MP] #86dwh7qkh Use gRPC when exporting traces

--- src/tracing.ts
+++ src/tracing.ts
@@ -6,7 +6,7 @@ import chalk from "chalk";
 import {escapeRegExp} from "lodash";
 import {NodeSDK} from "@opentelemetry/sdk-node";
 import {getNodeAutoInstrumentations} from "@opentelemetry/auto-instrumentations-node";
-import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-proto";
+import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-grpc";
 import {resourceFromAttributes} from "@opentelemetry/resources";
 import {ATTR_SERVICE_NAME} from "@opentelemetry/semantic-conventions";
 

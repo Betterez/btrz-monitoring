4a822ff9db7c29fbced01dd65a5a3b73bfa6c92c
Marc Peters <mr.marc.peters@gmail.com>
2025-05-30 14:29:47 -0700

    [MP] Export opentelemetry semantic attributes and custom attributes used by Betterez

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import * as semanticConventions from "@opentelemetry/semantic-conventions";
+
+export const monitoringAttributes = {
+  ATTR_BTRZ_ACCOUNT_ID: "btrz.account.id",
+  ...semanticConventions
+} as const;
+
 export {initializeTracing, trace, withTracing} from "./tracing";
